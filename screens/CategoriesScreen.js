27f75e136cbc1371ec3446eeb2a65c6a1268a053
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-03-21 18:40:55 +0700

    ^ grid styling and add category grid component

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native'
+import { StyleSheet, FlatList } from 'react-native'
 import { CATEGORIES } from '../data/dummy-data'
-import Color from '../constants/Color'
+import CategoryGridTile from '../components/CategoryGridTile'
 
 const CategoriesScreen = props => {
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity  style={styles.gridItem} onPress={() => {
+            <CategoryGridTile
+                title={itemData.item.title}
+                color={itemData.item.color}
+                onSelect={() => {
                 props.navigation.navigate('CategoryMeals', { categoryId: itemData.item.id})
-            }}>
-                <View>
-                    <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
+            }} />
         )
     }
 
@@ -30,11 +29,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150
     }
 })
 
