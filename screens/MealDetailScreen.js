5c63dc26271b6fec8b034e3b4bb805102bbb374c
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-03-22 09:26:04 +0700

    + header button component

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import {MEALS} from '../data/dummy-data'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import CustomHeaderButton from '../components/HeaderButton'
 
 const MealDetailScreen = props => {
     const mealId = props.navigation.getParam('mealId')
@@ -16,7 +18,12 @@ MealDetailScreen.navigationOptions = navigationData => {
     const mealId = navigationData.navigation.getParam('mealId')
     const selectedMeal = MEALS.find(meal => meal.id === mealId)
     return {
-        headerTitle: selectedMeal.title
+        headerTitle: selectedMeal.title,
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item title="Favorite" iconName='ios-star' onPress={() => console.log('Favorite')}/>
+            </HeaderButtons>
+        )
     }
 }
 
