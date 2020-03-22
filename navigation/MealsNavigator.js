559cc4500e5f213832890b9fcdc72b25eccd308c
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-03-22 09:55:36 +0700

    + bottom navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,12 @@
 import { Platform } from 'react-native'
 import Color from '../constants/Color'
 import { createStackNavigator } from 'react-navigation-stack'
-import {createAppContainer} from 'react-navigation'
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
+import FavoritesScreen from '../screens/FavoritesScreen'
 
 const MealsNavigator = createStackNavigator(
     {
@@ -22,4 +24,11 @@ const MealsNavigator = createStackNavigator(
     }
 )
 
-export default createAppContainer(MealsNavigator)
+const MealsFavTabNavigator = createBottomTabNavigator(
+    {
+        Meals: MealsNavigator,
+        Favorites: FavoritesScreen
+    }
+)
+
+export default createAppContainer(MealsFavTabNavigator)
