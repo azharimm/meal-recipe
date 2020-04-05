f7a20917953be62200da44bd7ec9f0d4693eff67
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-04-06 04:38:17 +0700

    ^ no favorite found text

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { View, Text, StyleSheet} from 'react-native'
 import { useSelector } from 'react-redux'
 import MealList from '../components/MealList'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import CustomHeaderButton from '../components/HeaderButton'
+import DefaultText from '../components/DefaultText'
 
 const FavoritesScreen = props => {
     const favMeals = useSelector(state => state.meals.favoriteMeals)
+    if(favMeals.length === 0 || !favMeals) {
+        return <View style={styles.content}>
+            <DefaultText>No Favorite Meals Found! Start Adding some.</DefaultText>
+        </View>
+    }
     return (
         <MealList listData={favMeals} navigation={props.navigation} />
     )
@@ -22,5 +29,13 @@ FavoritesScreen.navigationOptions = navData => {
 
     }
 }
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default FavoritesScreen
